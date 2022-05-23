307348df373a82178766348d45788d114cab79ff
Luzorb <55652894+Neryhm@users.noreply.github.com>
2022-05-24 00:42:01 +0700

    Update PythonLearning.py

--- PythonLearning.py
+++ PythonLearning.py
@@ -27,6 +27,7 @@
 # DRY principle - Don't Repeat Yourself
 # A bad code is said to abide by the WET principle - Write Everything Twice/We Enjoy Typing
 
+# # Function
 # def function(var):
 	# var+=1
 	# print(var)
@@ -66,3 +67,93 @@
 # temp=-10
 # assert(temp>=10),"Colder than absolute zero!"
 # Programmers often place assertions at the start of a function to check for valid input, and after a function call to check for valid output.t
+
+# f=open("Minhf.txt",'w+') 	#r:  Read, !Write, !Create, !Truncate, BEGINNING
+							# # r+: Read,  Write, !Create, !Truncate, BEGINNING
+							# # w: !Read,  Write,  Create,  Truncate, BEGINNING
+							# # w+: Read,  Write,  Create,  Truncate, BEGINNING
+							# # a: !Read,  Write,  Create, !Truncate, END
+							# # a+: Read,  Write,  Create, !Truncate, END
+# f.close()
+# with open("E:\\Develop\\Notepad++ Folders\\Python Core Leaning\\Minhf1.txt",'w+') as f1:
+	# f1.write("""Hello World!
+	# Hello Minh!
+	# Hello Earth!""")
+# with open("E:\\Develop\\Notepad++ Folders\\Python Core Leaning\\Minhf1.txt",'r+') as f1:
+	# print(f1.readlines(1))
+	# print(len(f1.read())) #this returns empty string if you already read all content in the file
+	
+# for _ in file:
+    # print(_[0]+str(len(_.strip())))  	#.strip() remove all preceding whitespace which include space,\t and \n
+										# Also, these 2 lines of code is the solution to the Book Titles problem on Sololearn
+
+# print(bool(None))
+# def func():
+	# print("Hi")
+# var=func()
+# print(var)  #this return None
+# print()		#this also return None
+
+# Birthdays={"Minh":1811,"Quyen":1301,}		#Only immutable objects can be used as keys
+# print(Birthdays)							#Mutable objects are: list and dict
+# Dict.get(key,text if not found)
+
+# Tuples: Lists but immutable
+# tuple="one","two","three"	#parentheses is optinal
+
+# List Slice: list[begin:end:step]
+# List Comprehensions*: list=[i**2 for i in range(10) if i**2 %2 ==0]
+
+# # String Formatting
+# list=[1,2,3]
+# text="Numbers:{a}{x}{f}".format(f=list[0],a=list[1],x=list[2])
+# msg="Numbers:{0}{1}{2}".format(list[0],list[1],list[2])
+# combine="Numbers:{0}{m}{1}{q}{2}".format(list[0],list[1],list[2],m="minh",q="quyen")
+# print(msg)
+# print(text)
+# print(combine)
+# name = input("Your name: ")
+# age = int(input("Your age: "))
+# print("{name} is {age} years old".format(name=name,age=age))
+
+# # String Functions
+# print(", ".join(["a","b","c"]))
+# print("Hello Minh".replace("Minh","world"))
+# print("This is a sentence.".startswith("his"))		#This returns "False" since the string starts with "This" not "his"
+# print("This is a sentence.".endswith("sentence.")) 	#This returns "True" since the string really ends with "sentence."
+# print("This is a sentence.".upper())
+# # Numeric Functions
+# print(min(sum([11,22]),max(abs(-30),2)))
+# # List Functions
+# print(all([i>3 for i in range(5)]))
+# print(any([i>3 for i in range(5)]))
+# for _ in enumerate([10,20,30]):
+	# print(_)
+
+# # Text Analyze
+# with open("TestingText.txt","r+") as f:
+	# text=f.read()
+# def count_char(text,char):
+	# return text.count(char)
+# print(count_char(text,"r"))
+# for char in "abcdefghijklmnopqrstuvwxyz":
+  # perc = 100 * count_char(text, char) / len(text)
+  # print("{0} - {1}%".format(char, round(perc, 2)))
+  
+# Longest Word Problem (Sololearn)
+# non-pythonic:
+txt = input()
+words=txt.split()
+max_char=0
+for i in words:
+    if len(i)>max_char:
+        max_char=len(i)
+        longestword=i
+print(longestword)
+# pythonic:
+text = input().split()
+length = [len(x) for x in text]
+print(text[length.index(max(length))])
+# pythonic ver.2:
+text = input().split()
+print(max(text, key=len))
